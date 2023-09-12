e365c1f732e07df107db1fe84a9ccb9c21e37212
Claudia-Merchan <cmerchan2002@gmail.com>
2023-09-12 12:24:07 -0500

    Added a multiplication method

--- apps.py
+++ apps.py
@@ -1,3 +1,9 @@
-def sum_List(givenList):
+def sumList(givenList):
     Sum = sum(givenList)
     return Sum
+
+def multiplyList(givenList):
+    product = 1
+    for n in givenList:
+        product = product * n
+    return product
